ec8038a7704069a6f5ab0ebb02fe510b075885cf
matrix505 <matthewsuarez40@gmail.com>
2025-10-15 15:00:58 +0800

    New commit

--- Stack.py
+++ Stack.py
@@ -3,23 +3,24 @@ class Node:
 		self.data = data
 		self.next = None
 class Stack:
-	def __init__(self):
-		self.top = None
-
-	def push(self, element):
-		new_node = Node(element)
-		new_node.next = self.top
-		self.top = new_node
-	def pop(self):
-		if self.top is None:
-			print("Stack Underflow")
-			return -1  # Return a sentinel value indicating error
-		element = self.top.data
-		self.top = self.top.next
-		return element
-
-	def peek(self):
-		if self.top is None:
-			print("Stack is empty")
-			return -1  # Return a sentinel value indicating error
-		return self.top.data
+    def __init__(self,name):
+        self.name = name # name per stack for tower indication
+        self.top = None
+        
+    def push(self, element):
+        new_node = Node(element)
+        new_node.next = self.top
+        self.top = new_node
+    def pop(self):
+        if self.top is None:
+            print("Stack Underflow")
+            return -1  # Return a sentinel value indicating error
+        element = self.top.data
+        self.top = self.top.next
+        return element
+    
+    def peek(self):
+        if self.top is None:
+            print("Stack is empty")
+            return -1  # Return a sentinel value indicating error
+        return self.top.data
---
+++ testing.py
@@ -0,0 +1,13 @@
+import Stack 
+
+disk = [i for i in range(3,11)]
+stack_A = Stack()
+stack_B = Stack()
+stack_C = Stack()
+
+
+
+def exampleDisk(diskNum:int) -> None:
+    pass
+
+exampleDisk(disk[0])
